bbbf5a00581b5114b57e35383ff03ace4a0a25df
valex <mval.fx@gmail.com>
2024-10-15 17:33:19 +0200

    Complete implementation of calculator with fully functional keyboard and mouse event listeners.

--- script.js
+++ script.js
@@ -10,156 +10,92 @@ let log = document.querySelector(".log");
 let calcNumbers = [];
 for (let i = 0; i < 10; i++) {
   calcNumbers[i] = document.getElementById([i] + "_button");
-
   // Click event listener
   calcNumbers[i].addEventListener("click", function () {
-    workingArray.push(i);
-    display.textContent = workingArray.join("");
+    numberEventHandler(i);
   });
-
-  // Key down event listener
+  // Keyboard event listener
   document.addEventListener("keydown", function (event) {
     if (event.key === i.toString()) {
-      workingArray.push(i);
-      display.textContent = workingArray.join("");
+      numberEventHandler(i);
     }
   });
 }
-// EVENT LISTENERS - the code that executes if event happens is the same - frame it into a function
-let plus = document.getElementById("add_button");
-// CLICK EVENT LISTENER
-plus.addEventListener("click", function () {
-  number = joinArray(workingArray);
-  operator = "+";
-  storingArray.push(number, operator);
-  workingArray.splice(0, workingArray.length);
-  log.textContent = storingArray.join(" ");
+
+// Decimal number (.) event listeners
+let dot = document.getElementById("dot_button");
+dot.addEventListener("click", function () {
+  // Before pushing it check if dot has already been used
+  if (!workingArray.includes(".")) {
+    dotEventHandler();
+  }
 });
-// KEYBOARD EVENT LISTENER
+document.addEventListener("keydown", function (event) {
+  if (event.key === ".") {
+    if (!workingArray.includes(".")) {
+      dotEventHandler();
+    }
+  }
+});
+
+// OPERATORS event listeners (click + keydown)
+let plus = document.getElementById("add_button");
+plus.addEventListener("click", function () {
+  plusEventHandler();
+});
 document.addEventListener("keydown", function (event) {
   if (event.key === "+") {
-    number = joinArray(workingArray);
-    operator = "+";
-    storingArray.push(number, operator);
-    workingArray.splice(0, workingArray.length);
-    log.textContent = storingArray.join(" ");
+    plusEventHandler();
   }
 });
 
 let minus = document.getElementById("minus_button");
-// CLICK EVENT LISTENER
 minus.addEventListener("click", function () {
-  number = joinArray(workingArray);
-  operator = "-";
-  storingArray.push(number, operator);
-  workingArray.splice(0, workingArray.length);
-  log.textContent = storingArray.join(" ");
+  minusEventHandler();
 });
-// KEYBOARD EVENT LISTENER
 document.addEventListener("keydown", function (event) {
   if (event.key === "-") {
-    number = joinArray(workingArray);
-    operator = "-";
-    storingArray.push(number, operator);
-    workingArray.splice(0, workingArray.length);
-    log.textContent = storingArray.join(" ");
+    minusEventHandler();
   }
 });
 
 let star = document.getElementById("star_button");
-// CLICK EVENT LISTENER
 star.addEventListener("click", function () {
-  number = joinArray(workingArray);
-  operator = "*";
-  storingArray.push(number, operator);
-  workingArray.splice(0, workingArray.length);
-  log.textContent = storingArray.join(" ");
+  starEventHandler();
 });
 document.addEventListener("keydown", function (event) {
   if (event.key === "*") {
-    number = joinArray(workingArray);
-    operator = "*";
-    storingArray.push(number, operator);
-    workingArray.splice(0, workingArray.length);
-    log.textContent = storingArray.join(" ");
+    starEventHandler();
   }
 });
 
 let slash = document.getElementById("divide_button");
-// CLICK EVENT LISTENER
 slash.addEventListener("click", function () {
-  number = joinArray(workingArray);
-  operator = "/";
-  storingArray.push(number, operator);
-  workingArray.splice(0, workingArray.length);
-  log.textContent = storingArray.join(" ");
+  slashEventHandler();
 });
 document.addEventListener("keydown", function (event) {
   if (event.key === "/") {
-    number = joinArray(workingArray);
-    operator = "/";
-    storingArray.push(number, operator);
-    workingArray.splice(0, workingArray.length);
-    log.textContent = storingArray.join(" ");
+    slashEventHandler();
   }
 });
 
-let dot = document.getElementById("dot_button");
-dot.addEventListener("click", function () {
-  // Before pushing it into an array - check if there already is a dot in the array
-  if (!workingArray.includes(".")) {
-    workingArray.push(".");
-    display.textContent = workingArray.join("");
-  }
-});
-
-document.addEventListener("keydown", function (event) {
-  if (event.key === ".") {
-    if (!workingArray.includes(".")) {
-      workingArray.push(".");
-      display.textContent = workingArray.join("");
-    }
-  }
-});
-
+// Equal sign can ONLY be pressed on a calculator
 let equal = document.getElementById("equal_button");
 equal.addEventListener("click", function () {
-  number = joinArray(workingArray);
-  storingArray.push(number);
-  console.log("UPDATED STORING ARRAY", storingArray);
-  // CALL GIVEN FUNCTION BASED ON OPERAND
-  operate(Number(storingArray[0]), Number(storingArray[2]), operator);
-  log.textContent = storingArray.join(" ");
-  operator = 0;
-  // Clear working array
-  workingArray.splice(0, workingArray.length);
-  console.log("CLEARED WORKING ARRAY AFTER RESULT", workingArray);
-  storingArray.splice(0, storingArray.length);
-  console.log("CLEARED STORING ARRAY AFTER RESULT", storingArray);
+  equalEventHandler();
 });
 
-// CLEAR BUTTON - Everything to default values
 let clearButton = document.getElementById("clear_button");
 clearButton.addEventListener("click", function () {
-  workingArray.splice(0, workingArray.length);
-  storingArray.splice(0, storingArray.length);
-  operator = 0;
-  number = 0;
-  display.textContent = 0;
-  log.textContent = "";
+  clearEventHandler();
 });
 
 let deleteButton = document.getElementById("delete_button");
 deleteButton.addEventListener("click", function () {
-  workingArray.pop();
-  display.textContent = workingArray.join("");
-
-  // Check if array is empty -> show 0
-  if (workingArray[0] === undefined) {
-    display.textContent = 0;
-  }
+  deleteEventHandler();
 });
 
+// Operation functions
 function joinArray(workingArray) {
   return workingArray.join("");
 }
@@ -192,6 +128,81 @@ function multiply(a, b) {
 
 function divide(a, b) {
   display.textContent = a / b;
+}
+
+// EVENT HANDLER FUNCTIONS
+function numberEventHandler(num) {
+  workingArray.push(num);
+  display.textContent = workingArray.join("");
+}
+
+function plusEventHandler() {
+  number = joinArray(workingArray);
+  operator = "+";
+  storingArray.push(number, operator);
+  workingArray.splice(0, workingArray.length);
+  log.textContent = storingArray.join(" ");
+}
+
+function minusEventHandler() {
+  number = joinArray(workingArray);
+  operator = "-";
+  storingArray.push(number, operator);
+  workingArray.splice(0, workingArray.length);
+  log.textContent = storingArray.join(" ");
+}
+
+function starEventHandler() {
+  number = joinArray(workingArray);
+  operator = "*";
+  storingArray.push(number, operator);
+  workingArray.splice(0, workingArray.length);
+  log.textContent = storingArray.join(" ");
+}
+
+function slashEventHandler() {
+  number = joinArray(workingArray);
+  operator = "/";
+  storingArray.push(number, operator);
+  workingArray.splice(0, workingArray.length);
+  log.textContent = storingArray.join(" ");
+}
+
+function dotEventHandler() {
+  workingArray.push(".");
+  display.textContent = workingArray.join("");
+}
+
+function equalEventHandler() {
+  number = joinArray(workingArray);
+  storingArray.push(number);
+  //console.log("UPDATED STORING ARRAY", storingArray);
+  operate(Number(storingArray[0]), Number(storingArray[2]), operator);
+  log.textContent = storingArray.join(" ");
+  operator = 0;
+  // Clear working array
+  workingArray.splice(0, workingArray.length);
+  console.log("CLEARED WORKING ARRAY AFTER RESULT", workingArray);
+  storingArray.splice(0, storingArray.length);
+  console.log("CLEARED STORING ARRAY AFTER RESULT", storingArray);
+}
+
+function clearEventHandler() {
+  workingArray.splice(0, workingArray.length);
+  storingArray.splice(0, storingArray.length);
+  operator = 0;
+  number = 0;
+  display.textContent = 0;
+  log.textContent = "";
+}
+
+function deleteEventHandler() {
+  workingArray.pop();
+  display.textContent = workingArray.join("");
+  // Check if array is empty -> show 0
+  if (workingArray[0] === undefined) {
+    display.textContent = 0;
+  }
 }
 
 // NEXT TASKS:
